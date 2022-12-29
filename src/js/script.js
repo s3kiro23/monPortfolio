ab93cb1eda040945220bfff0423039702e03245e
Jm GERONIMI <spawn2x@protonmail.com>
2022-12-29 12:44:30 +0100

    [UPD] main content + links

--- src/js/script.js
+++ src/js/script.js
@@ -10,34 +10,5 @@
         })
     }
 
-    // for (let link of offcanvasLinks)
-    // {
-    //     link.addEventListener('click', function(){
-    //         if (link.classList.contains('P1'))
-    //         {
-    //             window.open("https://webd-project.netlify.app", "_blank") 
-    //         }
-    //         else if (link.classList.contains('P2'))
-    //         {
-    //             window.open("https://webd-project.netlify.app", "_blank")
-    //         }
-    //         else if (link.classList.contains('P3'))
-    //         {
-    //             window.open("https://instantcafe.fun", "_blank")
-    //         }
-    //         else if (link.classList.contains('P4'))
-    //         {
-    //             window.open("https://flask-aflokkat-app.herokuapp.com/login", "_blank")
-    //         }
-    //         else if (link.classList.contains('P5'))
-    //         {
-    //             window.open("https://morpion-canvas.netlify.app", "_blank")
-    //         }
-    //         else if (link.classList.contains('P6'))
-    //         {
-    //             window.location="#"
-    //         }            
-    //     })
-    // }
 })()
 
