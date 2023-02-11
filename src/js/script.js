1fd43a8876184fc1cc43c75474e5c5431aa35bc1
Jm GERONIMI <spawn2x@protonmail.com>
2023-02-11 12:47:50 +0100

    [UPD] picture size and script JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
-(function(){
+// (function(){
     
-    // let offcanvasLinks = document.querySelectorAll("a")
-    let subTitles = document.querySelectorAll('h2');
+//     // let offcanvasLinks = document.querySelectorAll("a")
+//     let subTitles = document.querySelectorAll('h2');
     
-    for (let h2 of subTitles){
+//     for (let h2 of subTitles){
 
-        h2.addEventListener("mouseover", function(e){
-            console.log('Trouvé !')
-        })
-    }
+//         h2.addEventListener("mouseover", function(e){
+//             console.log('Trouvé !')
+//         })
+//     }
 
-})()
+// })()
 
