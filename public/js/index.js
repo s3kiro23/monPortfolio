934944f85edfde28bef2458bd3dd608e768f41ce
Jm GERONIMI <spawn2x@protonmail.com>
2023-02-11 20:18:04 +0100

    [ADD] contact mailing function + htaccess

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,51 @@
 $(function () {
-   btnToTop()
+	btnToTop();
+});
+
+let formContact = function () {
+    $('#btn-submit').prop('disabled', true);
+	let inputValues = {};
+	$("#flexCheckDefault:checked").val()
+		? (inputValues["cgu"] = $("#flexCheckDefault:checked").val())
+		: (inputValues["cgu"] = "");
+	$(".field").each(function () {
+		inputValues[$(this).attr("id")] = $(this).val();
+	});
+	$.ajax({
+		url: "../src/Controller/Contact.php",
+		dataType: "JSON",
+		type: "POST",
+		data: {
+			request: "contact",
+			values: inputValues,
+		},
+		success: function (response) {
+			toastMixin.fire({
+				animation: true,
+				title: response["msg"],
+				icon: "success",
+                width: 500,
+			});
+		},
+		error: function (jqxhr, textStatus, errorThrown) {
+			console.log(jqxhr);
+			console.log(textStatus);
+			console.log(errorThrown);
+		},
+	});
+};
+
+let toastMixin = Swal.mixin({
+	toast: true,
+	icon: "success",
+	title: "General Title",
+	animation: false,
+	position: "center",
+	showConfirmButton: false,
+	timer: 3000,
+	timerProgressBar: true,
+	didOpen: (toast) => {
+		toast.addEventListener("mouseenter", Swal.stopTimer);
+		toast.addEventListener("mouseleave", Swal.resumeTimer);
+	},
 });
