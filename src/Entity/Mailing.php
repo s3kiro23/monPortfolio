92d2763e6816b43ad1a2b26820520b7aeb96e071
Jm GERONIMI <spawn2x@protonmail.com>
2023-02-11 21:41:33 +0100

    [FIX] path document root for autoload

--- src/Entity/Mailing.php
+++ src/Entity/Mailing.php
@@ -5,7 +5,7 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
-require $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
+require __DIR__.'/../../vendor/autoload.php';
 
 class Mailing
 {
